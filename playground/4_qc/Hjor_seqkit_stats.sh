d725b3b1a9048cf73df9c52b29c81f5726e208d1
felipevzps <felipe.vzps@gmail.com>
2024-02-26 23:34:01 -0300

    Hjor seqkit stats

---
+++ playground/4_qc/Hjor_seqkit_stats.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#$ -q all.q
+#$ -V
+#$ -cwd
+#$ -pe smp 10
+
+module load miniconda3
+conda activate metagenomics
+
+/usr/bin/time -v seqkit stats ../3_adapter_removal/soft_filter/SRR11673974_1.trimmed.highquality.noadapter.fastq --threads $NSLOTS
