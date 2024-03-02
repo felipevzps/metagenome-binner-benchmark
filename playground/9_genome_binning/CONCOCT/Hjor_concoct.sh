c90bb601b3e4e7ce97f54f2537651b4d4945cb8b
felipevzps <felipe.vzps@gmail.com>
2024-03-02 18:05:30 -0300

    fixed

--- playground/9_genome_binning/CONCOCT/Hjor_concoct.sh
+++ playground/9_genome_binning/CONCOCT/Hjor_concoct.sh
@@ -3,7 +3,7 @@
 #$ -q all.q
 #$ -V
 #$ -cwd
-#$ -pe smp 10
+#$ -pe smp 5
 
 module load miniconda3
 conda activate metagenomics
@@ -11,18 +11,18 @@ conda activate metagenomics
 assembly=`ls -1 ../../5_assembly/Hjor/assembly.fasta`
 
 # Cut contigs into smaller parts
-/usr/bin/time -v cut_up_fasta.py $assembly -c 10000 -o 0 --merge_last -b Hjor_contigs_10K.bed > Hjor_contigs_10K.fa
+#/usr/bin/time -v cut_up_fasta.py $assembly -c 10000 -o 0 --merge_last -b Hjor_contigs_10K.bed > Hjor_contigs_10K.fa
 
 # Generate table with coverage depth
-/usr/bin/time -v concoct_coverage_table.py Hjor_contigs_10K.bed ../mapping/Hjor_mapped_sorted.bam > Hjor_coverage_table.tsv
+#/usr/bin/time -v concoct_coverage_table.py Hjor_contigs_10K.bed ../mapping/Hjor_mapped_sorted.bam > Hjor_coverage_table.tsv
 
 # Run COCOCT
-/usr/bin/time -v concoct --composition_file Hjor_contigs_10K.fa --coverage_file Hjor_coverage_table.tsv -b Hjor_concoct_output/ -t $NSLOTS
+#/usr/bin/time -v concoct --composition_file Hjor_contigs_10K.fa --coverage_file Hjor_coverage_table.tsv -b Hjor_concoct_output/ -t $NSLOTS
 
 # Merge subcontig clustering into original contig clustering:
-/usr/bin/time -v merge_cutup_clustering.py Hjor_concoct_output/clustering_gt1000.csv > Hjor_concoct_output/clustering_merged.csv
+#/usr/bin/time -v merge_cutup_clustering.py Hjor_concoct_output/clustering_gt1000.csv > Hjor_concoct_output/clustering_merged.csv
 
 # Extract bins as individual FASTA
-mkdir bins
+mkdir Hjor_bins
 /usr/bin/time -v extract_fasta_bins.py $assembly Hjor_concoct_output/clustering_merged.csv --output_path Hjor_bins
 
