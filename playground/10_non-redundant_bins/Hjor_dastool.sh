9d6962ef80e9efd5a898acd251755420604a92bb
felipevzps <felipe.vzps@gmail.com>
2024-03-02 18:06:02 -0300

    Hjor dastool

--- playground/10_non-redundant_bins/Hjor_dastool.sh
+++ playground/10_non-redundant_bins/Hjor_dastool.sh
@@ -12,4 +12,4 @@ assembly=`ls -1 ../5_assembly/Hjor/assembly.fasta`
 
 mkdir Hjor_output
 
-/usr/bin/time -v DAS_Tool -i Hjor_sample_data/MetaBAT2.contigs2bin.tsv,Hjor_sample_data/MaxBin2.contigs2bin.tsv,Hjor_sample_data/CONCOCT.contigs2bin.tsv -l MetaBAT2,MaxBin2,CONCOCT -c $assembly -o Hjor_output/DASToolRun -t $NSLOTS --write_bin_evals --write_bins --score_threshold 0.5 
+/usr/bin/time -v DAS_Tool -i Hjor_sample_data/MetaBAT2.contigs2bin.tsv,Hjor_sample_data/MaxBin2.contigs2bin.tsv,Hjor_sample_data/CONCOCT.contigs2bin.tsv,Hjor_sample_data/GraphMB.contigs2bin.tsv -l MetaBAT2,MaxBin2,CONCOCT,GraphMB -c $assembly -o Hjor_output/DASToolRun -t $NSLOTS --write_bin_evals --write_bins --score_threshold 0.5 
