7734d30ff777290ce28581b974f3a1ccb72ff135
felipevzps <felipe.vzps@gmail.com>
2024-03-02 18:05:50 -0300

    Damh dastool

--- playground/10_non-redundant_bins/Damh_dastool.sh
+++ playground/10_non-redundant_bins/Damh_dastool.sh
@@ -12,4 +12,4 @@ assembly=`ls -1 ../5_assembly/Damh/assembly.fasta`
 
 mkdir Damh_output
 
-/usr/bin/time -v DAS_Tool -i Damh_sample_data/MetaBAT2.contigs2bin.tsv,Damh_sample_data/MaxBin2.contigs2bin.tsv,Damh_sample_data/CONCOCT.contigs2bin.tsv -l MetaBAT2,MaxBin2,CONCOCT -c $assembly -o Damh_output/DASToolRun -t $NSLOTS --write_bin_evals --write_bins --score_threshold 0.5 
+/usr/bin/time -v DAS_Tool -i Damh_sample_data/MetaBAT2.contigs2bin.tsv,Damh_sample_data/MaxBin2.contigs2bin.tsv,Damh_sample_data/CONCOCT.contigs2bin.tsv,Damh_sample_data/GraphMB.contigs2bin.tsv -l MetaBAT2,MaxBin2,CONCOCT,GraphMB -c $assembly -o Damh_output/DASToolRun -t $NSLOTS --write_bin_evals --write_bins --score_threshold 0.5 
