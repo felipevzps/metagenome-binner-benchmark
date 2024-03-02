d87ec7a018cd439def90bddb770b788aad162b2a
felipevzps <felipe.vzps@gmail.com>
2024-03-02 18:05:12 -0300

    fixed

--- playground/9_genome_binning/GraphMB/Hjor_graphmb.sh
+++ playground/9_genome_binning/GraphMB/Hjor_graphmb.sh
@@ -8,7 +8,6 @@
 module load miniconda3
 conda activate metagenomics
 
-Hjor=`ls -1 ../../5_assembly/Hjor/assembly.fasta`
-Hjor_depth=`ls -1 ../MetaBAT2/Hjor_depth.txt`
+Hjor=Hjor/
 
-/usr/bin/time -v graphmb --assembly $Hjor --outdir Hjor_bins/ --assembly_name assembly.fasta --graph_file assembly_graph.gfa --depth $Hjor_depth --numcores $NSLOTS
+/usr/bin/time -v graphmb --assembly $Hjor --outdir Hjor_bins/ --assembly_name assembly.fasta --graph_file assembly_graph.gfa --depth Hjor_depth.txt --numcores $NSLOTS --contignodes --writebins 
